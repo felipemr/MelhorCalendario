c29c6a74c0235a375d772b3838a0f0f175e63f3e
felipemr <felipemr94@outlook.com>
2015-03-26 17:44:00 -0300

    melhor de todos

---
+++ MelhorHistorico/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MelhorHistorico
+//
+//  Created by Felipe Marques Ramos on 25/03/15.
+//  Copyright (c) 2015 Felipe Marques Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
